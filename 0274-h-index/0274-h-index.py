b1311b04acda3496a7f320a3ce21afa5ddafab7a
Narendra Singh dangi <82656649+narendrasinghdangi@users.noreply.github.com>
2023-07-03 00:02:34 +0530

    Time: 51 ms (75.35%), Space: 16.5 MB (90.14%) - LeetHub

--- 0274-h-index/0274-h-index.py
+++ 0274-h-index/0274-h-index.py
@@ -1,15 +1,7 @@
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
-        if citations==[0] or citations==[0,0] or citations== [0,0,0]:
-            return 0
-        citations.sort()
-        for i in range(len(citations),0,-1):
-            h=i
-            c=0
-            j=len(citations)-1
-            while citations[j]>=h and j>=0:
-                c=c+1
-                j=j-1
-            if c>=h:
-                return h
-            
+        citations.sort(reverse=True)
+        for i in range(len(citations)):
+            if citations[i]<=i:
+                return i
+        return len(citations)
