5883f3fbca7828615ddb4ba029ff674ab54d7b3f
Narendra Singh dangi <82656649+narendrasinghdangi@users.noreply.github.com>
2023-07-02 19:57:24 +0530

    Time: 79 ms (47.26%), Space: 17.7 MB (15.70%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -2,6 +2,5 @@ class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         s=0
         for a, b in pairwise(prices):
-            print(a,b)
             s=s+max(b - a, 0)
         return s
