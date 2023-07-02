e6ef1530b5b74e5cceabea0fb940293036202e04
Narendra Singh dangi <82656649+narendrasinghdangi@users.noreply.github.com>
2023-07-02 12:52:23 +0530

    Time: 128 ms (67.26%), Space: 17.7 MB (97.07%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.py
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -1,12 +1,7 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
         li=[]
-        d={}
-        for i in nums:
-            if i not in d:
-                d[i]=1
-            else:
-                d[i]=d[i]+1
+        d=Counter(nums)
         for i in d:
             if d[i]>len(nums)//3:
                 li.append(i)
