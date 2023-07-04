cd5e19eab7787c4841274fb741d1a93cd9926433
Narendra Singh dangi <82656649+narendrasinghdangi@users.noreply.github.com>
2023-07-04 15:35:03 +0530

    Time: 46 ms (74.25%), Space: 16.3 MB (66.21%) - LeetHub

--- 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.py
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.py
@@ -1,14 +1,7 @@
 class Solution:
     def trailingZeroes(self, n: int) -> int:
-        s=1
-        for i in range(1,n+1):
-            s=s*i
-        t=0
-        while s>=0:
-            i=s%10
-            if i==0:
-                t=t+1
-            else:
-                break
-            s=s//10
-        return t
+        co = 0
+        while n > 0:
+            co+= n // 5
+            n //= 5
+        return co
