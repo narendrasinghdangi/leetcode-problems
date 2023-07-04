124b2b99077a0f29f228e8f1e3427b8674e0c88f
Narendra Singh dangi <82656649+narendrasinghdangi@users.noreply.github.com>
2023-07-04 21:52:48 +0530

    Time: 55 ms (91.70%), Space: 16.7 MB (29.10%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,10 +1,8 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        ransomNote = list(ransomNote)
-        magazine = list(magazine)
         for i in ransomNote:
             if i not in magazine:
                 return False
             else:
-                magazine.remove(i)
+                magazine=magazine.replace(i,"",1)
         return True
