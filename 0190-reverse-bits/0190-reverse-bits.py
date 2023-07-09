a1a1b51d4538ef4730b172a2cc0390034e29aa45
Narendra Singh dangi <82656649+narendrasinghdangi@users.noreply.github.com>
2023-07-09 16:36:32 +0530

    Time: 34 ms (99.04%), Space: 16.2 MB (62.67%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.py
+++ 0190-reverse-bits/0190-reverse-bits.py
@@ -1,8 +1,6 @@
 class Solution:
     def reverseBits(self, n: int) -> int:
-        b=bin(n)[2:]
-        b=b[::-1]
+        b=bin(n)[2:][::-1]
         for i in range(32-len(b)):
             b=b+"0"
-        b=int(b,2)
-        return b
+        return int(b,2)
