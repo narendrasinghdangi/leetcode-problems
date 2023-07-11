ddc19d7ab611f31e93e0509d7145d285997fbbcf
Narendra Singh dangi <82656649+narendrasinghdangi@users.noreply.github.com>
2023-07-12 00:52:20 +0530

    Time: 858 ms (35.66%), Space: 16.5 MB (47.88%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.py
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.py
@@ -5,7 +5,6 @@ class Solution:
         arr.append(0)
         dp=[0]*(len(arr)+1)
         dp[0]=0
-        print(dp)
         for i in range(1,k+1):
             dp[i]=max(arr[:i])*i
         for i in range(k,len(arr)):
@@ -13,7 +12,6 @@ class Solution:
             for j in range(k):
                 lol.append(dp[i-j]+max(arr[i-j:i+1])*(j+1))
             dp[i+1]=max(lol)
-        print(dp)
         return dp[-2]
             
             
