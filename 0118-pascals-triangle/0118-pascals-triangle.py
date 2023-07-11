2e08997281755e2f9c041de695d8bd1cae0918c8
Narendra Singh dangi <82656649+narendrasinghdangi@users.noreply.github.com>
2023-07-11 05:41:45 +0530

    Time: 46 ms (75.59%), Space: 16.3 MB (41.15%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.py
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -3,9 +3,9 @@ class Solution:
         if numRows==1:
             return [[1]]
         li=[[1],[1,1]]
-        for i in range(1,numRows-1):
+        for i in range(2,numRows):
             lol=[1]
-            for j in range(i):
+            for j in range(i-1):
                 lol.append(li[-1][j]+li[-1][j+1])
             lol.append(1)
             li.append(lol)
