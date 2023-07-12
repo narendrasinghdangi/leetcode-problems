5589cdf0a986e43c92a1457dd8d74da4a9dd5786
Narendra Singh dangi <82656649+narendrasinghdangi@users.noreply.github.com>
2023-07-12 19:01:03 +0530

    Time: 63 ms (82.82%), Space: 16.4 MB (21.23%) - LeetHub

--- 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.py
+++ 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.py
@@ -1,6 +1,6 @@
 class Solution:
     def distributeCookies(self, cookies: List[int], k: int) -> int:
-        def dfs(cookies, index,children):
+        def dfs(index,children):
             nonlocal ans
             if index == len(cookies):
                 unfairness = max(children)
@@ -8,11 +8,11 @@ class Solution:
                 return
             for i in range(len(children)):
                 children[i] += cookies[index]
-                dfs(cookies, index+1, children)
+                dfs(index+1, children)
                 children[i] -= cookies[index]
                 if children[i] == 0: break
 
         ans = float('inf')
         children = [0]*k
-        dfs(cookies, 0, children)
+        dfs(0, children)
         return ans
