7e3143aeb0566c26b5bdac658eaac443e6a4eb24
Narendra Singh dangi <82656649+narendrasinghdangi@users.noreply.github.com>
2023-07-12 19:02:25 +0530

    Time: 50 ms (93.84%), Space: 16.4 MB (58.47%) - LeetHub

--- 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.py
+++ 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.py
@@ -3,14 +3,14 @@ class Solution:
         def dfs(index,children):
             nonlocal ans
             if index == len(cookies):
-                unfairness = max(children)
-                ans = min(ans, unfairness)
+                ans = min(ans, max(children))
                 return
             for i in range(len(children)):
                 children[i] += cookies[index]
                 dfs(index+1, children)
                 children[i] -= cookies[index]
-                if children[i] == 0: break
+                if children[i] == 0:
+                    break
 
         ans = float('inf')
         children = [0]*k
