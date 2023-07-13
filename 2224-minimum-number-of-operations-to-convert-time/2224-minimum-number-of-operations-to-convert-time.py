ee5b3aa245446f0393b8e79856ef36a678000240
Narendra Singh dangi <82656649+narendrasinghdangi@users.noreply.github.com>
2023-07-13 19:52:02 +0530

    Time: 47 ms (61.94%), Space: 16.4 MB (39.68%) - LeetHub

--- 2224-minimum-number-of-operations-to-convert-time/2224-minimum-number-of-operations-to-convert-time.py
+++ 2224-minimum-number-of-operations-to-convert-time/2224-minimum-number-of-operations-to-convert-time.py
@@ -2,14 +2,14 @@ class Solution:
     def convertTime(self, current: str, correct: str) -> int:
         current=int(current[:2])*60+int(current[3:])
         correct=int(correct[:2])*60+int(correct[3:])
-        c=0
         lol=correct-current
-        li=[60,15,5,1]
-        while lol!=0:
-            for i in li:
-                if lol>=i:
-                    c=c+1
-                    lol=lol-i
-                    break
-        return c
+        a=lol//60
+        lol=lol-60*a
+        b=lol//15
+        lol=lol-15*b
+        c=lol//5
+        lol=lol-5*c
+        return a+b+c+lol
+            
+     
         
