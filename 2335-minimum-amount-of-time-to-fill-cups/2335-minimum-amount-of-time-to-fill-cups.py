d21c9b1f05f15b91021d9854feff8faeed6d12ce
Narendra Singh dangi <82656649+narendrasinghdangi@users.noreply.github.com>
2023-07-13 22:14:49 +0530

    Time: 58 ms (11.50%), Space: 16.4 MB (31.42%) - LeetHub

--- 2335-minimum-amount-of-time-to-fill-cups/2335-minimum-amount-of-time-to-fill-cups.py
+++ 2335-minimum-amount-of-time-to-fill-cups/2335-minimum-amount-of-time-to-fill-cups.py
@@ -2,25 +2,11 @@ class Solution:
     def fillCups(self, amount: List[int]) -> int:
         c=0
         while set(amount)!={0}:
-            m=float("inf")
-            for i in amount:
-                if i<m and i!=0:
-                    m=i
-            m=amount.index(min(amount))
-            if m==0:
-                if amount[1]!=0:
-                    amount[1]=amount[1]-1
-                if amount[2]!=0:
-                    amount[2]=amount[2]-1
-            elif m==1:
-                if amount[0]!=0:
-                    amount[0]=amount[0]-1
-                if amount[2]!=0:
-                    amount[2]=amount[2]-1
-            else:
-                if amount[0]!=0:
-                    amount[0]=amount[0]-1
-                if amount[1]!=0:
-                    amount[1]=amount[1]-1
+            m=amount.index(max(amount))
+            amount[m]=amount[m]-1
+            for i in range(3):
+                if amount[i]!=0 and i!=m:
+                    amount[i]=amount[i]-1
+                    break
             c=c+1
         return c
