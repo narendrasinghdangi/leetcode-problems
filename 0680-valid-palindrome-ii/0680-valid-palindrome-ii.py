bd695e69bcf44ff0a5f5be723adc1c7d006bdeaf
Narendra Singh dangi <82656649+narendrasinghdangi@users.noreply.github.com>
2023-07-14 03:22:32 +0530

    Time: 106 ms (91.36%), Space: 16.8 MB (79.22%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
@@ -1,7 +1,7 @@
 class Solution:
     def validPalindrome(self, s: str) -> bool:
         for i in range(len(s) // 2):
-            if s[i] != s[~i]:
+            if s[i] != s[-i-1]:
                 l = s[i:~i]
                 r = s[i+1:len(s)-i]
                 return l == l[::-1] or r == r[::-1]
