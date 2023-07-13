62d142002a0315515f1adaf922cb73c07e0e06bb
Narendra Singh dangi <82656649+narendrasinghdangi@users.noreply.github.com>
2023-07-14 03:27:26 +0530

    Time: 117 ms (77.44%), Space: 16.9 MB (41.33%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
@@ -3,6 +3,7 @@ class Solution:
         for i in range(len(s) // 2):
             if s[i] != s[-i-1]:
                 l = s[i:~i]
+                print(l)
                 r = s[i+1:len(s)-i]
                 return l == l[::-1] or r == r[::-1]
         return True
