5a725b83e614e640cd5725987400b07be948301e
Narendra Singh dangi <82656649+narendrasinghdangi@users.noreply.github.com>
2023-07-13 16:28:45 +0530

    Time: 106 ms (10.65%), Space: 16.2 MB (92.01%) - LeetHub

---
+++ 1403-minimum-subsequence-in-non-increasing-order/1403-minimum-subsequence-in-non-increasing-order.py
@@ -0,0 +1,10 @@
+class Solution:
+    def minSubsequence(self, nums: List[int]) -> List[int]:
+        if len(nums)==1:
+            return nums
+        nums.sort(reverse=True)
+        for i in range(1,len(nums)+1):
+            if sum(nums[:i])>sum(nums[i:]):
+                return nums[:i]
+            
+        
