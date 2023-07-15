3d8f4ede69093622e9188328811c83542f73baac
Narendra Singh dangi <82656649+narendrasinghdangi@users.noreply.github.com>
2023-07-16 00:09:14 +0530

    Time: 909 ms (50.14%), Space: 24.3 MB (64.47%) - LeetHub

---
+++ 2279-maximum-bags-with-full-capacity-of-rocks/2279-maximum-bags-with-full-capacity-of-rocks.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maximumBags(self, capacity: List[int], rocks: List[int], additionalRocks: int) -> int:
+        c=0
+        dp=[0]*len(rocks)
+        for i in range(len(rocks)):
+            dp[i]=capacity[i]-rocks[i]
+        dp.sort()
+        print(dp)
+        for i in range(len(dp)):
+            if dp[i]==0:
+                c=c+1
+            elif additionalRocks>=dp[i]:
+                additionalRocks=additionalRocks-dp[i]
+                c=c+1
+        return c
+            
