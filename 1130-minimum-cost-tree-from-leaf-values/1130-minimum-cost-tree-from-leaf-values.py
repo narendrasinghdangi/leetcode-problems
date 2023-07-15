dcb3a70938bd753bfa6bc110db1826129e18f293
Narendra Singh dangi <82656649+narendrasinghdangi@users.noreply.github.com>
2023-07-15 23:21:33 +0530

    Time: 320 ms (5.17%), Space: 16.8 MB (20.16%) - LeetHub

--- 1130-minimum-cost-tree-from-leaf-values/1130-minimum-cost-tree-from-leaf-values.py
+++ 1130-minimum-cost-tree-from-leaf-values/1130-minimum-cost-tree-from-leaf-values.py
@@ -7,15 +7,13 @@ class Solution:
                 return 0 
             if (left,right) in dp:
                 return dp[(left,right)]
-            if left + 1 == right:
-                return arr[left] * arr[right]
             
             min_cost = float('inf')
             for p in range(left,right):
-                cost = (max(arr[left:p+1]) * max(arr[p+1:right+1])) + helper(left,p) + helper(p+1,right)
+                cost = max(arr[left:p+1]) * max(arr[p+1:right+1]) + helper(left,p) + helper(p+1,right)
                 min_cost = min(min_cost,cost)
             
-            dp[(left,right)] = min_cost
+                dp[(left,right)] = min_cost
             return min_cost
         
         
