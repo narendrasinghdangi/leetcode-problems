818f9de3fef9de3ce7c91090789fd3420ee8f4c1
Narendra Singh dangi <82656649+narendrasinghdangi@users.noreply.github.com>
2023-07-17 15:58:12 +0530

    Time: 1975 ms (5.49%), Space: 27.5 MB (8.24%) - LeetHub

--- 1558-minimum-numbers-of-function-calls-to-make-target-array/1558-minimum-numbers-of-function-calls-to-make-target-array.py
+++ 1558-minimum-numbers-of-function-calls-to-make-target-array/1558-minimum-numbers-of-function-calls-to-make-target-array.py
@@ -1,7 +1,8 @@
 class Solution:
     def minOperations(self, nums: List[int]) -> int:
         c=0
-        while set(nums)!={0}:
+        arr = [0]*len(nums)
+        while nums[:]!=arr[:]:
             even=True
             for i in range(len(nums)):
                 if nums[i]%2!=0:
