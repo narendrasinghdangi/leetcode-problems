4e17b4b38d558528badfb7994cd3ff5ab7d7577b
Narendra Singh dangi <82656649+narendrasinghdangi@users.noreply.github.com>
2023-07-21 00:12:31 +0530

    Time: 117 ms (40.52%), Space: 22.4 MB (7.13%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -1,20 +1,15 @@
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
-        dp={}
         n=len(grid)
         m=len(grid[0])
+        @lru_cache(None)
+        
         def dfs(i,j):
-            lol=0
-            if (i,j) in dp:
-                return dp[(i,j)]
             if i==0 and j==0:
                 return grid[i][j]
             if i<0 or j<0:
                 return float("inf")
-            lol=grid[i][j]+min(dfs(i-1,j),dfs(i,j-1))
-            dp[(i,j)]=lol
-            grid[i][j]=lol
-            return grid[i][j]
+            return grid[i][j]+min(dfs(i-1,j),dfs(i,j-1))
                 
             
         return dfs(n-1,m-1)
