cc6c3e911c7bec2348ef282b263899a29536e81a
Narendra Singh dangi <82656649+narendrasinghdangi@users.noreply.github.com>
2023-07-20 23:56:59 +0530

    Time: 48 ms (55.69%), Space: 16.2 MB (81.21%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,18 +1,3 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        a=m+n-2
-        b=m-1
-        d=n-1
-        c=1
-        e=1
-        for i in range(1,a+1):
-            c=c*i
-        for i in range(1,b+1):
-            e=e*i
-        c=c/e
-        e=1
-        for i in range(1,d+1):
-            e=e*i
-        
-        return round(c/e)
-            
+        return int(math.factorial(m+n-2) / (math.factorial(m-1)*math.factorial(n-1)))
