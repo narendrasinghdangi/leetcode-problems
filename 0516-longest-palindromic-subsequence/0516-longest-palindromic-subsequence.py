a467f0138c723fd7f514b2ec7d4cf32387e4ca95
Narendra Singh dangi <82656649+narendrasinghdangi@users.noreply.github.com>
2023-07-24 20:02:51 +0530

    Time: 2582 ms (36.31%), Space: 41.7 MB (45.01%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.py
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.py
@@ -1,5 +1,8 @@
 class Solution:
-    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+    def longestPalindromeSubseq(self, s: str) -> int:
+        
+        text1=s
+        text2=s[::-1]
         dp=[[0]*(len(text2)+1) for _ in range(len(text1)+1)]
         for i in range(len(text1)):
             for j in range(len(text2)):
@@ -8,9 +11,3 @@ class Solution:
                 else:
                     dp[i+1][j+1]=max(dp[i][j+1],dp[i+1][j])
         return dp[len(text1)][len(text2)]
-    
-    
-    def longestPalindromeSubseq(self, s: str) -> int:
-        return self.longestCommonSubsequence(s,s[::-1])
-        
-    
