552e120f3baa3703be1584b0b4ee27a1a4e49456
Narendra Singh dangi <82656649+narendrasinghdangi@users.noreply.github.com>
2023-07-31 15:34:40 +0530

    Time: 67 ms (93.50%), Space: 16.9 MB (15.45%) - LeetHub

--- 1947-maximum-compatibility-score-sum/1947-maximum-compatibility-score-sum.py
+++ 1947-maximum-compatibility-score-sum/1947-maximum-compatibility-score-sum.py
@@ -1,24 +1,19 @@
 class Solution:
     def maxCompatibilitySum(self, students: List[List[int]], mentors: List[List[int]]) -> int:
         n = len(students)
-        
         def get_score(m, s):
             c = 0
             for i in range(len(m)):
                 if m[i] == s[i]:
                     c += 1
-            
             return c
         
         @cache
         def dp(curr_index_student, tuple_ment):
-            
             if curr_index_student == n:
                 return 0
-            
             list_ment = list(tuple_ment)
             m = -inf
-            
             for mentor_index in range(n):
                 if list_ment[mentor_index-1] == True:
                     continue
