ff301f3ab708895eda66fbe4b198b56cf0262d26
Narendra Singh dangi <82656649+narendrasinghdangi@users.noreply.github.com>
2023-08-17 03:43:06 +0530

    Time: 50 ms (59.66%), Space: 16.3 MB (96.20%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.py
+++ 0063-unique-paths-ii/0063-unique-paths-ii.py
@@ -1,23 +1,20 @@
 class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
-        m=len(obstacleGrid)
-        n=len(obstacleGrid[0])
-        dp=[[0 for _ in range(n)] for _ in range(m)]
-        for col in range(n):
-            if obstacleGrid[0][col]==1:
-                break
-            dp[0][col]=1
-        for row in range(m):
-            if obstacleGrid[row][0]==1:
+        
+        dp=[[0 for _ in range(len(obstacleGrid[0]))] for _ in range(len(obstacleGrid))]
+        for i in range(len(obstacleGrid)):
+            if obstacleGrid[i][0]==1:
                 break
 
-            dp[row][0]=1
+            dp[i][0]=1
+        for j in range(len(obstacleGrid[0])):
+            if obstacleGrid[0][j]==1:
+                break
 
-        for row in range(1,m):
-            for col in range(1,n):
-                if obstacleGrid[row][col]==1:
-                    continue
-
-                dp[row][col]=dp[row-1][col]+dp[row][col-1]
-
-        return dp[-1][-1]   
+            dp[0][j]=1
+                
+        for i in range(1,len(obstacleGrid)):
+            for j in range(1,len(obstacleGrid[0])):
+                if obstacleGrid[i][j]!=1:
+                    dp[i][j]=dp[i][j-1]+dp[i-1][j]
+        return dp[-1][-1]
