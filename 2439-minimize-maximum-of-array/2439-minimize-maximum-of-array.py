2f57222dae31601d36b0cf07124fec6ee51cff34
Narendra Singh dangi <82656649+narendrasinghdangi@users.noreply.github.com>
2023-09-11 18:28:04 +0530

    Time: 651 ms (85.64%), Space: 29.2 MB (92.33%) - LeetHub

--- 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.py
+++ 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.py
@@ -1,23 +1,8 @@
 class Solution:
     def minimizeArrayValue(self, nums: List[int]) -> int:
-        start, end = 0, max(nums)
-        while start + 1 < end:
-            mid = (start + end) // 2
-            if self.check(nums, mid):
-                end = mid
-            else:
-                start = mid
-        if self.check(nums, start):
-            return start
-        else:
-            return end
-        
-    def check(self, nums, k):
-        n = len(nums)
-        temp = 0
-        for i in range(n - 1, 0, -1):
-            if temp + nums[i] > k:
-                temp += nums[i] - k
-            else:
-                temp = 0
-        return False if temp + nums[0] > k else True
+        total_sum = 0
+        result = 0
+        for index in range(len(nums)):
+            total_sum += nums[index]
+            result = max(result, (total_sum + index) // (index + 1))
+        return int(result)
