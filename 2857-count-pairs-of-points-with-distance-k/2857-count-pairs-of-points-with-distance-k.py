1858dd5d20d5487c642752f4616f5a87de524473
Narendra Singh dangi <82656649+narendrasinghdangi@users.noreply.github.com>
2023-09-18 00:32:52 +0530

    Time: 1595 ms (100.00%), Space: 30.7 MB (100.00%) - LeetHub

---
+++ 2857-count-pairs-of-points-with-distance-k/2857-count-pairs-of-points-with-distance-k.py
@@ -0,0 +1,9 @@
+class Solution:
+    def countPairs(self, coordinates: List[List[int]], k: int) -> int:
+        count = Counter()
+        res = 0
+        for x1, y1 in coordinates:
+            for x in range(k + 1):
+                res += count[x1 ^ x, y1 ^ (k - x)]
+            count[x1, y1] += 1
+        return res
